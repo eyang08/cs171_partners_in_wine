cbe0620fd73a262551bf2fda91bed7a2b70eed27
Lizzy Yang <lizzyyang@janetyellensmbp.home>
2020-12-01 14:31:12 -0500

    made final directory

---
+++ FINAL_COMBINED/js/piechart.js
@@ -0,0 +1,141 @@
+/* * * * * * * * * * * * * *
+*         PieChart         *
+* * * * * * * * * * * * * */
+
+
+class PieChart {
+
+    // constructor method to initialize Timeline object
+    constructor(parentElement, wordData, varietyData, geoData, countryData) {
+        this.parentElement = parentElement;
+        this.wordData = wordData;
+        this.varietyData = varietyData;
+        this.geoData = geoData;
+        this.countryData = countryData;
+        this.displayData = [];
+        this.circleColors = ['#b2182b','#d6604d','#f4a582','#fddbc7'];
+
+        // call initVis method
+        this.initVis()
+    }
+
+    initVis(){
+        let vis = this;
+
+        // margin conventions
+        vis.margin = {top: 10, right: 50, bottom: 10, left: 50};
+        vis.width = $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right;
+        vis.height = $("#" + vis.parentElement).height() - vis.margin.top - vis.margin.bottom;
+
+        // init drawing area
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+        // create a projection
+        vis.projection = d3.geoOrthographic() // d3.geoStereographic()
+            .scale(100)
+            .translate([vis.width / 2, vis.height / 2])
+
+        // define a geo generator w/ predefined projection
+        vis.path = d3.geoPath()
+            .projection(vis.projection);
+
+        // sphere
+        vis.svg.append("path")
+            .datum({type: "Sphere"})
+            .attr("class", "graticule")
+            .attr('fill', '#ADDEFF')
+            .attr("stroke","rgba(129,129,129,0.35)")
+            .attr("d", vis.path);
+
+        // convert topojson into geojson
+        vis.world = topojson.feature(vis.geoData, vis.geoData.objects.countries).features
+
+        // draw countries
+        vis.countries = vis.svg.selectAll(".country")
+            .data(vis.world)
+            .enter().append("path")
+            .attr('class', 'pie-country')
+            .attr("d", vis.path)
+
+        // append tooltip
+        vis.tooltip = d3.select("body").append('div')
+            .attr('class', "tooltip")
+            .attr('id', 'mapTooltip')
+
+        // Draggable feature
+        let m0,
+            o0;
+
+        vis.svg.call(
+            d3.drag()
+                .on("start", function (event) {
+
+                    let lastRotationParams = vis.projection.rotate();
+                    m0 = [event.x, event.y];
+                    o0 = [-lastRotationParams[0], -lastRotationParams[1]];
+                })
+                .on("drag", function (event) {
+                    if (m0) {
+                        let m1 = [event.x, event.y],
+                            o1 = [o0[0] + (m0[0] - m1[0]) / 4, o0[1] + (m1[1] - m0[1]) / 4];
+                        vis.projection.rotate([-o1[0], -o1[1]]);
+                    }
+
+                    // Update the map
+                    vis.path = d3.geoPath().projection(vis.projection);
+                    d3.selectAll(".pie-country").attr("d", vis.path)
+                    d3.selectAll(".graticule").attr("d", vis.path)
+                })
+        )
+        // call next method in pipeline
+        this.wrangleData();
+    }
+
+    // wrangleData method
+    wrangleData(){
+        let vis = this
+        console.log("updating table")
+        vis.displayData = vis.varietyData.filter(function (d) {return d.variety == selectedVariety})
+
+        let tempData = vis.wordData.filter(function (d) {return d.variety == selectedVariety})
+        console.log(tempData)
+
+        let priceSum = 0;
+        let ratingSum = 0;
+        let sum = 0;
+
+        tempData.forEach( entry => {
+            priceSum += +entry['price'];
+            ratingSum += +entry['points'];
+            sum ++;
+        });
+
+        document.getElementById("price").innerHTML = priceSum/sum;
+        document.getElementById("rating").innerHTML = ratingSum/sum;
+
+        vis.updateVis()
+
+
+    }
+
+    // updateVis method
+    updateVis() {
+        let vis = this;
+        var percentFormatter = d3.format(".0%")
+        console.log(vis.displayData[0])
+        console.log(percentFormatter(vis.displayData[0].percent))
+        //add title
+        vis.svg.selectAll(".percent").remove()
+        vis.svg.append('g')
+            .append('text')
+            .attr("class", "percent")
+            .text(percentFormatter(vis.displayData[0].percent) + " grows in " + vis.displayData[0].location)
+            .attr('transform', `translate(${vis.width / 2}, 20)`)
+            .attr('text-anchor', 'middle');
+
+    }
+}
---
+++ FINAL_COMBINED/js/radarVis.js
@@ -0,0 +1,211 @@
+/* main JS file */
+
+
+
+class RadarVis {
+
+
+  constructor(_parentElement, _data, _top, title) {
+    this.parentElement = _parentElement;
+    this.data = _data;
+    this.top = _top
+    this.displayData = [];
+    this.title = title
+    this.initVis();
+  }
+
+  initVis(){
+    let vis = this;
+
+    vis.w = $(vis.parentElement).width() -200;
+    vis.h = $(vis.parentElement).width() -200;
+
+    /*let colorscale = d3.scale.category10();
+
+//Legend titlesl
+    let LegendOptions = ['Quality: 3','Quality: 4','Quality: 5', 'Quality: 6', 'Quality: 7', 'Quality: 8'];
+
+
+      let svg = d3.select('#body')
+          .selectAll('svg')
+          .append('svg')
+          .attr("width", vis.w+300)
+          .attr("height", vis.h)
+
+//Create the title for the legend
+      let text = svg.append("text")
+          .attr("class", "title")
+          .attr('transform', 'translate(90,0)')
+          .attr("x", w - 70)
+          .attr("y", 10)
+          .attr("font-size", "12px")
+          .attr("fill", "#404040")
+          .text("What % of owners use a specific service in a week");
+
+      //Initiate Legend
+      let legend = svg.append("g")
+          .attr("class", "legend")
+          .attr("height", 100)
+          .attr("width", 200)
+          .attr('transform', 'translate(90,20)')
+      ;
+//Create colour squares
+      legend.selectAll('rect')
+          .data(LegendOptions)
+          .enter()
+          .append("rect")
+          .attr("x", w - 65)
+          .attr("y", function(d, i){ return i * 20;})
+          .attr("width", 10)
+          .attr("height", 10)
+          .style("fill", function(d, i){ return colorscale(i);})
+      ;
+//Create text next to squares
+      legend.selectAll('text')
+          .data(LegendOptions)
+          .enter()
+          .append("text")
+          .attr("x", w - 52)
+          .attr("y", function(d, i){ return i * 20 + 9;})
+          .attr("font-size", "11px")
+          .attr("fill", "#737373")
+          .text(function(d) { return d; })
+      ;*/
+
+
+
+      vis.wrangleData()
+  }
+
+
+
+wrangleData(){
+
+    let vis = this;
+
+
+  vis.displayData = vis.data
+
+  if (vis.top){
+    vis.displayData.sort((a,b) => {return b.quality - a.quality})
+  } else {
+    vis.displayData.sort((a,b) => {return a.quality - b.quality})
+  }
+
+  vis.wines3 = vis.data.filter((wine => wine.quality == 3));
+  vis.wines4 = vis.data.filter((wine => wine.quality == 4));
+  vis.wines5 = vis.data.filter((wine => wine.quality == 5));
+  vis.wines6 = vis.data.filter((wine => wine.quality == 6));
+  vis.wines7 = vis.data.filter((wine => wine.quality == 7));
+  vis.wines8 = vis.data.filter((wine => wine.quality == 8));
+
+  vis.wines = [vis.wines3, vis.wines4, vis.wines5, vis.wines6, vis.wines7, vis.wines8];
+
+  vis.qualityData =[];
+
+  vis.wines.forEach(winedata=> {
+        function avg(data, attr){
+            let sum = 0;
+            for (let i = 0; i < data.length; i++) {
+                    sum = +(sum) + +((data[i][attr]));
+                }
+            let average = (sum/data.length) - 4
+            return average
+        }
+
+
+      vis.qualityData.push({
+          Quality: avg(winedata, "quality"),
+          alcohol: avg(winedata, "alcohol"),
+          pH: avg(winedata, "pH"),
+          density: avg(winedata, "density"),
+          resSugar: avg(winedata, "residual sugar"),
+          volAcid: avg(winedata, "volatile acidity"),
+          fixAcid: avg(winedata, "fixed acidity"),
+          citAcid: avg(winedata, "citric acid"),
+          chlorides: avg(winedata, "chlorides"),
+          totSulfur: avg(winedata, "total sulfur dioxide"),
+          freeSulfur: avg(winedata, "free sulfur dioxide"),
+          sulphates: avg(winedata, "sulphates")
+      })
+  })
+
+    console.log(vis.qualityData)
+
+    vis.finalData = [[],[],[],[],[],[]];
+
+    let i = 0;
+    vis.qualityData.forEach(wine => {
+        if (document.getElementById("attribute1").checked == true){
+            vis.finalData[i].push({axis: "Alcohol", value: (wine.alcohol) })
+        }
+        if (document.getElementById("attribute2").checked == true) {
+            vis.finalData[i].push({axis: "pH", value: (wine.pH)})
+        }
+        if (document.getElementById("attribute3").checked == true) {
+            vis.finalData[i].push({axis: "Density", value: (wine.density)})
+        }
+        if (document.getElementById("attribute4").checked == true) {
+            vis.finalData[i].push({axis: "Residual Sugar", value: wine.resSugar})
+        }
+        if (document.getElementById("attribute5").checked == true) {
+            vis.finalData[i].push({axis: "Volatile Acidity", value: wine.volAcid})
+        }
+        if (document.getElementById("attribute6").checked == true) {
+            vis.finalData[i].push({axis: "Fixed Acidity", value: wine.fixAcid})
+        }
+        if (document.getElementById("attribute7").checked == true) {
+            vis.finalData[i].push({axis: "Citric Acid", value: wine.citAcid})
+        }
+        if (document.getElementById("attribute8").checked == true) {
+            vis.finalData[i].push({axis: "Chlorides", value: wine.chlorides })
+        }
+        if (document.getElementById("attribute9").checked == true) {
+            vis.finalData[i].push({axis: "Free Sulfur Dioxide", value: wine.freeSulfur})
+        }
+        if (document.getElementById("attribute10").checked == true) {
+            vis.finalData[i].push({axis: "Total Sulfur Dioxide", value: wine.totSulfur})
+        }
+        if (document.getElementById("attribute11").checked == true) {
+            vis.finalData[i].push({axis: "Sulphates", value: wine.sulphates})
+        }
+        i++;
+    })
+
+    console.log(vis.finalData)
+    vis.updateVis()
+
+};
+
+
+
+
+updateVis () {
+  let vis = this;
+
+  vis.color = "crimson"
+
+  if (vis.data == whiteWine){
+      vis.color = "darksalmon"
+  }
+
+
+  console.log(vis.w)
+  vis.mycfg = {
+    w: vis.w,
+    h: vis.h,
+    maxValue: 2,
+    levels: 4,
+    ExtraWidthX: 300,
+      title: vis.title,
+      color: vis.color
+  }
+
+  console.log(vis.finalData)
+//Call function to draw the Radar chart
+//Will expect that data is in %'s
+  RadarChart.draw(vis.parentElement, vis.finalData, vis.mycfg);
+
+
+}
+};
